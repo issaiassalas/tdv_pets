d082a2781bb1546b10d9cf6abe42f47dc9bcc6dd
Isaias Salas <issaiassalas@gmail.com>
2022-05-30 09:39:28 -0400

    preparense para los problemas

--- models/pets.py
+++ models/pets.py
@@ -8,5 +8,6 @@ class Pet(models.Model):
     name = fields.Char(string = 'Nombre de la mascota', required = True)
     age = fields.Integer(string = 'Edad de la mascota')
     breed = fields.Char(string = 'Raza de la mascota', help='Aqui va la raza')
+    last_name = fields.Char(string = 'Apellido', default='Garcia')
 
     owner = fields.Many2one('res.partner', string='Propietario')
